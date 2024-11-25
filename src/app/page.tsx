9656c5d2c7fb9ab25ccdc662a1ecd0ef793239d3
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2024-11-25 18:00:31 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,7 @@ import React from "react";
 export default function Home() {
     return (
         <main>
-            <div className="flex min-h-screen flex-col items-center justify-between">
-                <div className="z- items-center justify-between font-mono text-sm flex">
-                    <OrganizationsOnGithub/>
-                </div>
-            </div>
+            <OrganizationsOnGithub/>
         </main>
     )
 }
