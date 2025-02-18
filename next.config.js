3d7e872c26c911cf13d6683349f1eb4dad1c0bbe
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2025-02-18 21:12:35 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
     images: {
         loader: 'akamai',
         path: '/'
